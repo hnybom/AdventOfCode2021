1c7f331c572be330ecdda62185bdeeae7fb0e5dd
Henri Nybom <henriny@pop-os.localdomain>
2021-12-01 08:28:07 +0200

    Solve day 1

---
+++ src/main/kotlin/fi/solita/hnybom/advent2021/Day1.kt
@@ -0,0 +1,26 @@
+package fi.solita.hnybom.advent2021
+
+import java.io.File
+
+class Day1 {
+
+    private val input: List<Long> = File(
+        "/home/henriny/work/own/AdventOfCode2021/src/main/resources/input1.txt").readLines().map { it.toLong() }
+
+    fun part1(): Long {
+        return input.zipWithNext().sumOf { (a, b) -> if (a < b) 1L else 0L }
+    }
+
+    fun part2(): Long {
+        return input.windowed(3, 1, false).zipWithNext().sumOf { (a, b) -> if (a.sum() < b.sum()) 1 else 0L }
+    }
+
+    fun solve() {
+        println("Part 1: ${part1()}")
+        println("Part 2: ${part2()}")
+    }
+}
+
+fun main(args: Array<String>) {
+    Day1().solve()
+}
---
+++ src/test/kotlin/fi/solita/hnybom/advent2021/Day1Test.kt
@@ -0,0 +1,4 @@
+package fi.solita.hnybom.advent2021
+
+class Day1Test {
+}
